b058f8ce99b8e2255b4b993db7e2ae2a004353f5
MatiScavuzzo <mati.ntvg@hotmail.com>
2024-03-13 19:13:38 -0300

    Updated

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  mode: 'production',
+  entry: './app/page.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'pomodoro.js',
+    library: 'pomodoroMS',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false }),
+  ],
+};
